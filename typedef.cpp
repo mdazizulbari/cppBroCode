238707ba274eea934cb60e4fd18a81ced333b1ac
smabari <mdazizulbarishafin@gmail.com>
2025-09-08 05:48:21 +0600

    typedef

---
+++ typedef.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+// typedef std::vector<std::pair<std::string, int>> pairlist_t;
+typedef std::string text_t;
+
+int main(){
+    // typedef = reserved keyword used to create an additional name
+    // (alias) for another data type.
+    // New identifier for an existing type
+    // Helps with readability and reduces typos
+
+    text_t firstName = "SMA";
+    std::string lastName = "bari";
+
+    std:: cout << firstName << " " << lastName << "\n";
+
+
+    return 0;
+}
