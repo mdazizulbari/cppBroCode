13b27e81787bcfcc1b91ef892f1acb377cfdcb61
smabari <mdazizulbarishafin@gmail.com>
2025-09-08 03:48:30 +0600

    variables

---
+++ varriables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+
+    int x; //declaration
+    x = 5; //assignment
+
+    // or
+    int y = 6;
+    
+    int sum = x + y;
+
+    std::cout << x << "\n";
+    std::cout << y << "\n";
+    std::cout << sum << "\n";
+
+    return 0;
+}
