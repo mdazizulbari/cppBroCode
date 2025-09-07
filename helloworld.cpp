1649ebb29a57cb0947b08e5bd62a4d362fca96f3
smabari <mdazizulbarishafin@gmail.com>
2025-09-08 03:10:00 +0600

    multi line comment

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,6 +3,14 @@
 int main(){
 
     // This is a Comment
+    /*
+    This 
+    is 
+    a 
+    multi line 
+    comment
+    */
+
    std::cout << "I love Chocolate" << "\n";
    std::cout << "It's really good!" << std::endl;
     return 0;
