3a134a5634d0c51e84af06adf61669b3004dd761
smabari <mdazizulbarishafin@gmail.com>
2025-09-08 04:17:53 +0600

    const definition

--- 4.const.cpp
+++ 4.const.cpp
@@ -2,6 +2,10 @@
 
 int main(){
 
+    // The const keyword specifies that a variable's value is constant
+    // tells the compiler to prevent anything from modifying it
+    // (read-only)
+
     const double PI = 3.14159;
     const int LIGHT_SPEED = 299792458;
 
