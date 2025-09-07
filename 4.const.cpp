5317884b73cf4fda9a9b59a3551d6c1fb12d8892
smabari <mdazizulbarishafin@gmail.com>
2025-09-08 04:30:17 +0600

    circumference

--- 4.const.cpp
+++ 4.const.cpp
@@ -9,5 +9,10 @@ int main(){
     const double PI = 3.14159;
     const int LIGHT_SPEED = 299792458;
 
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+
+    std::cout << circumference << "cm";
+
     return 0;
 }
