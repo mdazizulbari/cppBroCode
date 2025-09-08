dfff524d7c49b6341fe548e023f65b6c8cb7a992
smabari <mdazizulbarishafin@gmail.com>
2025-09-08 06:31:26 +0600

    7.arithmeticOperators

--- 7.arithmeticOperators.cpp
+++ 7.arithmeticOperators.cpp
@@ -2,7 +2,27 @@
 
 int main(){
 
-    // 37.45m
+    // double students = 20;
+    int students = 20;
+
+    // students = students +1;
+    // students+=1;
+    // students++;
+
+    // students = students - 2;
+    // students-=2;
+    // students--;
+
+    // students = students * 2;
+    // students*=2;
+
+    // students = students / 2;
+    // students/=3;
+
+    int remainder = students % 3;
+
+    std::cout << students;
+    std::cout << remainder;
 
     return 0;
 }
