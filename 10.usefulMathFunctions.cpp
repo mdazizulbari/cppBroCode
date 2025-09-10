62d0c1128dfe107acec25e8687497e4f88930ab7
smabari <mdazizulbarishafin@gmail.com>
2025-09-11 00:31:57 +0600

    10.usefulMathFunctions

---
+++ 10.usefulMathFunctions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+
+    double x = 3;
+    double y = 4;
+    double z;
+
+    // z = std::max(x, y);
+    // z = std::min(x, y);
+    // z = pow(2, 4);
+    // z = sqrt(9);
+    // z = abs(-3);
+    // there are more which i am not writing
+    // www.cplusplus.com/reference/cmath
+
+    std::cout << z << "\n";
+
+    return 0;
+}
